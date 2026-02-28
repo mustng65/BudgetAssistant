f62cb35e7b042472f95c0eb4359cdcd1ca1a7d16
David <mustng65@hotmail.com>
2026-02-28 15:19:01 -0800

    added blank tithingCalc page and route

--- budgetAssistant.Web/src/app/app.routes.ts
+++ budgetAssistant.Web/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'home', loadComponent: () => import('./common/home/home').then((m) => m.Home) },
+  {
+    path: 'tithingCalc',
+    loadComponent: () => import('./utilities/tithing-calc/tithing-calc').then((m) => m.TithingCalc),
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
 ];
---
+++ budgetAssistant.Web/src/app/utilities/tithing-calc/tithing-calc.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tithing-calc',
+  imports: [],
+  templateUrl: './tithing-calc.html',
+  styleUrl: './tithing-calc.scss',
+})
+export class TithingCalc {}
