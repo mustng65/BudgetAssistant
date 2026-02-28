00e53133dbf51b1b304bc7589127df6e3bfdfe24
David <mustng65@hotmail.com>
2026-02-28 15:06:33 -0800

    Feature/add inital UI (#1)
    * added navbar and home page
    
    * updated page title

--- budgetAssistant.Web/src/app/app.routes.ts
+++ budgetAssistant.Web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', loadComponent: () => import('./common/home/home').then((m) => m.Home) },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
