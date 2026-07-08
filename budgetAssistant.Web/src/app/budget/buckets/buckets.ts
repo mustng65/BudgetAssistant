5e10be12cbb4fe9f8c77dc3089a7242f10805aed
David <mustng65@hotmail.com>
2026-07-07 22:41:33 -0700

    started to add month picker for buckets

--- budgetAssistant.Web/src/app/budget/buckets/buckets.ts
+++ budgetAssistant.Web/src/app/budget/buckets/buckets.ts
@@ -1,31 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActualBudgetService, CategoryGroup } from '../../services/actual-budget.service';
 import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
+import { DateTime } from 'luxon';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-buckets',
-  imports: [MatCardModule, CurrencyPipe],
+  imports: [
+    MatCardModule,
+    CurrencyPipe,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './buckets.html',
   styleUrl: './buckets.scss',
 })
-export class Buckets {
+export class Buckets implements OnInit {
   bucketGroup?: CategoryGroup;
+  date!: FormControl<DateTime | null>;
 
-  constructor(private budget: ActualBudgetService) {
-    budget.getBudget(2026, 6).subscribe((res) => {
+  constructor(private budget: ActualBudgetService) {}
+  ngOnInit(): void {
+    this.date = new FormControl<DateTime>(DateTime.now().minus({ month: 1 }));
+
+    this.loadBuckets(this.date.value!);
+
+    // this.date.valueChanges.subscribe((value) => {
+    //   console.log('Date Changed: ' + value?.toFormat('MM/dd/yyyy'));
+    //   if (value == null) return;
+
+    //   this.loadBuckets(value);
+    // });
+  }
+
+  loadBuckets(value: DateTime<boolean>) {
+    const year = value.year;
+    const month = value.month;
+
+    this.budget.getBudget(year, month).subscribe((res) => {
       const group = res.categoryGroups.filter((group) => group['name'] === 'Buckets')[0];
 
-      budget.getCategoryGroups(2026, 6).subscribe((categories) => {
+      console.log('Got group');
+
+      this.budget.getCategoryGroups(year, month).subscribe((categories) => {
+        console.log('Got group - transactions');
+
         group.categories.forEach((category) => {
-          const group = categories.filter((g) => g['name'] === category.name)[0];
-          if (group) {
-            category.transactions = group.transactions;
+          const groupWithTransactions = categories.filter((g) => g['name'] === category.name)[0];
+
+          if (groupWithTransactions) {
+            console.log('found matching group: ' + category.name);
+            category.transactions = groupWithTransactions.transactions;
           }
         });
-
+        console.log('Set bucket group - start');
         this.bucketGroup = group;
+        console.log('Set bucket group - end');
       });
     });
+  }
+
+  setMonthAndYear(normalizedMonthAndYear: DateTime, datepicker: MatDatepicker<DateTime>) {
+    const ctrlValue = DateTime.fromObject({
+      month: normalizedMonthAndYear.month,
+      year: normalizedMonthAndYear.year,
+    });
+    datepicker.close();
+    this.date.setValue(ctrlValue);
   }
 }
