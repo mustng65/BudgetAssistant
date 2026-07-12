9d60f0c1a873bbfea510abdcdf7f6fadd31fa41b
David <mustng65@hotmail.com>
2026-07-12 15:54:17 -0700

    added some missing items

--- budgetAssistant.Web/src/app/planning/saving-spending/saving-spending.ts
+++ budgetAssistant.Web/src/app/planning/saving-spending/saving-spending.ts
@@ -1,5 +1,10 @@
 import { Component, computed, signal } from '@angular/core';
-import { ActualBudgetService, Account, CategoryGroup } from '../../services/actual-budget.service';
+import {
+  ActualBudgetService,
+  Account,
+  CategoryGroup,
+  Category,
+} from '../../services/actual-budget.service';
 import { CurrencyPipe } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { forkJoin } from 'rxjs';
@@ -14,9 +19,15 @@ import { FormsModule } from '@angular/forms';
 export class SavingSpending {
   savingsAccount = signal<Account | null>(null);
   newSavingsRemainingBalance = signal<number>(0);
+  additionalCategoriesBalance = signal<number>(0);
+
+  startingTotal = signal<number>(0);
+  currentTotal = signal<number>(0);
+  newTotal = signal<number>(0);
 
   startingCategoryGroups = signal<CategoryGroup[]>([]);
   currentCategoryGroups = signal<CategoryGroup[]>([]);
+  additionalCategories = signal<Category[]>([]);
 
   savingsRemainingBalance = computed<number>(() => {
     const totalBalance = this.currentCategoryGroups().reduce((total, group) => {
@@ -30,10 +41,15 @@ export class SavingSpending {
     console.log(
       `starting total balance: ${totalBalance} | Current Balance: ${this.savingsAccount()?.balance_current}`,
     );
-    return (this.savingsAccount()?.balance_current || 0) - totalBalance;
+    return (
+      (this.savingsAccount()?.balance_current || 0) -
+      totalBalance -
+      this.additionalCategoriesBalance()
+    );
   });
 
   savingSpendingGroups = [
+    'Donations',
     'Travel',
     'Office',
     'Misc',
@@ -54,16 +70,27 @@ export class SavingSpending {
       budget.getBudget(today.getFullYear(), today.getMonth() + 1),
     ]).subscribe({
       next: ([start, current]) => {
-        const startSavingsCategories = start.categoryGroups.filter(
-          (group) => this.savingSpendingGroups.find((g) => g == group.name) != null,
+        this.startingCategoryGroups.set(this.getSavingSpendingGroups(start.categoryGroups));
+        this.currentCategoryGroups.set(this.getSavingSpendingGroups(current.categoryGroups));
+
+        const additionalCategories: Category[] = [];
+
+        current.categoryGroups.forEach((group) => {
+          const found = group.categories.find((category) =>
+            ['Minimum Savings Balance', 'New Car'].find((g) => g == category.name),
+          );
+          if (found) additionalCategories.push(found);
+        });
+        console.log(additionalCategories);
+
+        this.additionalCategories.set(additionalCategories);
+        this.additionalCategoriesBalance.set(
+          additionalCategories.reduce((total, item) => total + item.balance, 0),
         );
 
-        const currentSavingsCategories = current.categoryGroups.filter(
-          (group) => this.savingSpendingGroups.find((g) => g == group.name) != null,
-        );
+        this.startingTotal.set(this.calcAllCategoryGroupsTotal(this.startingCategoryGroups()));
+        this.currentTotal.set(this.calcAllCategoryGroupsTotal(this.currentCategoryGroups()));
 
-        this.startingCategoryGroups.set(startSavingsCategories);
-        this.currentCategoryGroups.set(currentSavingsCategories);
         this.updateNewBalance();
       },
       error: (err) => {
@@ -94,8 +121,33 @@ export class SavingSpending {
       );
     }, 0);
 
+    const additionalCategoriesTotalBalance = this.additionalCategories().reduce(
+      (total, category) => {
+        return (
+          total + category.balance + (category.addedAmount ? category.addedAmount : 0) * 100
+        );
+      },
+      0,
+    );
+
     this.newSavingsRemainingBalance.set(
-      (this.savingsAccount()?.balance_current || 0) - totalBalance,
+      (this.savingsAccount()?.balance_current || 0) -
+        totalBalance -
+        additionalCategoriesTotalBalance,
+    );
+  }
+
+  calcAllCategoryGroupsTotal(categoryGroups: CategoryGroup[]) {
+    return categoryGroups.reduce((total, group) => {
+      return (
+        total + group.categories.reduce((subtotal, category) => subtotal + category.balance, 0)
+      );
+    }, 0);
+  }
+
+  getSavingSpendingGroups(allGroup: CategoryGroup[]) {
+    return allGroup.filter(
+      (group) => this.savingSpendingGroups.find((g) => g == group.name) != null,
     );
   }
 }
